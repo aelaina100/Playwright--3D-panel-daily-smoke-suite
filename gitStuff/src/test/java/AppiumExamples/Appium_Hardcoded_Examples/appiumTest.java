6f8002e8525e21dde308ba27624f26063dd2e200
aelaina100 <aelaina100@gmail.com>
2024-11-22 02:06:46 -0500

    Creating an Appium Java project. Documenting Appium architecture and beginning to include instructions on all required software installation and necessary System Environment Variables configurations

---
+++ gitStuff/src/test/java/AppiumExamples/Appium_Hardcoded_Examples/appiumTest.java
@@ -0,0 +1,20 @@
+package AppiumExamples.Appium_Hardcoded_Examples;
+
+
+
+public class appiumTest {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Apium test&&&");
+
+	}
+	
+	/*
+	@Test
+	public void someAppiumTest()
+	{
+		System.out.println("TestNG Compiler");
+	}
+  */
+}

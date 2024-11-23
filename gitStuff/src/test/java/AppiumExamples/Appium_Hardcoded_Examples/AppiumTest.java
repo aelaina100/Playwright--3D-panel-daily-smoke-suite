d076d2ac8a6053b67b71addaf2d988f7e266e1e7
aelaina100 <aelaina100@gmail.com>
2024-11-23 00:23:59 -0500

    Building the Appium project and adding set up notes

---
+++ gitStuff/src/test/java/AppiumExamples/Appium_Hardcoded_Examples/AppiumTest.java
@@ -0,0 +1,13 @@
+package AppiumExamples.Appium_Hardcoded_Examples;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void appiumTesting()
+	{
+		System.out.println("Apium*****************");
+	}
+
+}

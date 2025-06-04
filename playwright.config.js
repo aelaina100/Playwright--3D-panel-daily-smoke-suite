31aef5960e6263c2bdc9594bb1a22af174be75ef
aelaina100 <aelaina100@gmail.com>
2025-06-03 23:57:46 -0400

    Ready to push code for the 2nd pull request. All tests are working as expected

---
+++ playwright.config.js
@@ -0,0 +1,44 @@
+
+import { defineConfig, devices } from '@playwright/test';
+
+const Config = ({
+  testDir: './tests',
+  retries:3, 
+  timeout: 40 * 1000,      
+
+  expect: {
+    timeout: 20 * 1000, 
+          },
+
+  reporter: 'html',
+  
+  projects: [ 
+    
+    {
+      name: 'Chrome execution',
+      use: {
+        browserName: 'chromium',   
+        headless: true,    
+        screenshot: 'on',  // 'only-on-failure',   
+        video: 'retain-on-failure', // or 'on-first-retry' -Recording a video only when re-trying a test for the first time.
+        ignoreHttpsErrors: true,  
+        permissions: ['geolocation'],  
+        trace: 'retain-on-failure', 
+        // To maximize a browser window:
+        viewport:null,
+        launchOptions: {
+          args: ["--start-maximized"],
+        }
+        
+      }
+    },
+     ]
+});
+
+export default defineConfig({
+  reporter: [  //This reporter is that of Slack
+    ['html'],
+    ['json', { outputFile: 'test-results.json' }]
+  ],
+});
+module.exports = Config 
